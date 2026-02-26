7d51f734d2ebf8bfbda9aa3707bf82b8ba9184a7
Enrique Arendse <enriquearendse031@gmail.com>
2026-02-26 22:19:23 +0200

    Add project files.

---
+++ EventEase_POE/Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using EventEase_POE.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventEase_POE.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Venue> Venues { get; set; }
+		public DbSet<Event> Events { get; set; }
+		public DbSet<Booking> Bookings { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			// Prevent double booking
+			modelBuilder.Entity<Booking>()
+				.HasIndex(b => new { b.VenueId, b.BookingDate })
+				.IsUnique();
+
+			// Prevent cascade delete
+			modelBuilder.Entity<Booking>()
+				.HasOne(b => b.Venue)
+				.WithMany(v => v.Bookings)
+				.HasForeignKey(b => b.VenueId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+			modelBuilder.Entity<Booking>()
+				.HasOne(b => b.Event)
+				.WithMany(e => e.Bookings)
+				.HasForeignKey(b => b.EventId)
+				.OnDelete(DeleteBehavior.Restrict);
+		}
+	}
+}
